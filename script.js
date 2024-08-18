1f2566a472a26f24a1c6efcc6a36522f7378401f
Danielzapirtan <137394552+Danielzapirtan@users.noreply.github.com>
2024-08-18 10:56:14 +0300

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ let soldFinal;
 let stopLoss;
 let table;
 let tradeIndex;
-
+log.innerHTML = ``;
 const tabs = document.querySelectorAll(".tab");
 const tabContent = document.querySelectorAll(".tab-pane");
 
