986b755fd98fae4d90b19571bffc070c88c3e61c
Antoniu-Daniel Zăpîrțan <antoniudanielzapirtan@gmail.com>
2024-09-11 11:17:42 +0000

    Use ma

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const context = canvas.getContext("2d");
 const log = document.getElementById("log");
 let stockData;
 
+const shorts = 12;
+const longs = 26;
 let positionSize;
 let entryDate;
 let entryPrice;
