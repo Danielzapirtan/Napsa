72c44de01340b543ce21eced003bb86ee6aba871
Danielzapirtan <antoniudanielzapirtan@gmail.com>
2024-10-12 17:44:06 +0300

    Back to safety

--- script.js
+++ script.js
@@ -428,8 +428,8 @@ function downloadJSON() {
         return response.text();
       })
       .then((data) => {
-        //stockData = JSON.parse(data);
-	stockData = getMockData();
+        stockData = JSON.parse(data);
+	//stockData = getMockData();
         const stockElement = createTable(JSON.stringify(stockData));
         const dataElement = document.getElementById("Data");
         removeAllChildren(dataElement);
